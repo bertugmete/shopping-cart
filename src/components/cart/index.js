eb3ea8abab5119d87b6dce4d9409dd2ae91d2f84
bertugmete1 <bertu-mete@hotmail.com>
2020-11-10 11:15:41 +0300

    cart component added

---
+++ src/components/cart/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { formatCurrency } from "../../utils";
+import "./assets/style.scss";
+
+export default class Cart extends Component {
+  handleRemove = (_id) => {
+    this.props.removeFromCart(_id);
+  };
+
+  renderCartList = () => {
+    return (
+      <div className="cart__list">
+        <ul>
+          {this.props.cartList.map((cartItem) => {
+            return (
+              <li>
+                <div className="product__info">
+                  <img src={cartItem.image} alt={cartItem.title} />
+                  <span>{cartItem.title}</span>
+                </div>
+                <div className="actions">
+                  <span>
+                    {formatCurrency(cartItem.price)} x {cartItem.amount}
+                  </span>
+                  <button
+                    className="button button__remove"
+                    onClick={() => this.handleRemove(cartItem._id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  };
+
+  renderTotalPrice = () => {
+    return (
+      <div className="total__cart">
+        <div className="total__price">
+          <span>Total:</span>
+          <span>
+            {formatCurrency(
+              this.props.cartList.reduce((a, c) => a + c.price * c.amount, 0)
+            )}
+          </span>
+        </div>
+        <button className="button button__proceed">Proceed</button>
+      </div>
+    );
+  };
+
+  render() {
+    let headerText =
+      this.props.cartList.length > 0
+        ? `You have ${this.props.cartList.reduce((a, c) => a + c.amount, 0)} in
+      the cart`
+        : "Add prodcut to your cart";
+    return (
+      <div className="cart">
+        <div className="cart__header">
+          <span>{headerText}</span>
+        </div>
+        {this.props.cartList.length > 0 && (
+          <React.Fragment>
+            {this.renderCartList()}
+            {this.renderTotalPrice()}
+          </React.Fragment>
+        )}
+      </div>
+    );
+  }
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 export function formatCurrency(money) {
-  return `${money}₺`;
+  return `${money.toFixed(2)}₺`;
 }
